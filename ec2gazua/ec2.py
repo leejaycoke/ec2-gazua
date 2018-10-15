d5bba08b1ab7cf93310a7fa4e2119f126b4f7cfb
JuHyun Lee <leejaycoke@gmail.com>
2018-10-15 15:29:01 +0900

    ASG로 증가된 intsance(추정) 누락건 수정

--- ec2gazua/ec2.py
+++ ec2gazua/ec2.py
@@ -57,8 +57,10 @@ class EC2InstanceLoader(object):
 
         client = session.client('ec2')
 
-        return [i['Instances'][0] for i in
-                client.describe_instances()['Reservations']]
+        instances = []
+        for revs in client.describe_instances()['Reservations']:
+            instances += revs['Instances']
+        return instances
 
     def load_all(self):
         manager = EC2InstanceManager()
@@ -71,7 +73,7 @@ class EC2InstanceLoader(object):
                 ec2_instance = EC2Instance(self.config[aws_name], aws_instance)
 
                 if self.config[aws_name]['filter'][
-                    'connectable'] and not ec2_instance.is_connectable:
+                        'connectable'] and not ec2_instance.is_connectable:
                     continue
 
                 manager.add_instance(aws_name, ec2_instance.group,
@@ -190,4 +192,4 @@ class EC2Instance(object):
     @property
     def is_connectable(self):
         return self.is_running and self.has_key_file and \
-               self.connect_ip is not None
+            self.connect_ip is not None
