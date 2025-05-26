c59fe906129f235ac5f6455a7c247e41ee040c28
이주현 <jhlee@ban-life.com>
2025-05-26 13:35:11 +0900

    version 0.1.0 -> 0.2.0

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open('LICENSE') as f:
 
 setup(
     name='ec2-gazua',
-    version='0.1.0',
+    version='0.2.0',
     description='Easy accessing EC2 SSH through tmux',
     author='leejaycoke',
     author_email='leejaycoke@gmail.com',
